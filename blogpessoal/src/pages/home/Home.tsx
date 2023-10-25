cc8d77b3b7d726a3a13db4c0dcb1bfaa29dfa5f1
udsoncosta <udsoncostasantana@gmail.com>
2023-10-25 15:56:01 -0300

    Trabalhando com Tailwind

--- blogpessoal/src/pages/home/Home.tsx
+++ blogpessoal/src/pages/home/Home.tsx
@@ -1,22 +1,31 @@
-import './Home.css'
 
 function Home() {
     return (
         <>
-            <div className='corpo'>
-                <div>
-                    <div className='paragrafo'>
-                        <h2 className='titulo'>Seja Bem Vindo ao meu Blog Pessoal!</h2>
-                        <br></br>
-                        <p>Compartilhe aqui suas experiências e curiosidades!</p>
-                    </div>
-                    <br></br>
+            <div className="bg-indigo-900 flex justify-center">
+                <div className='container grid grid-cols-2 text-white'>
+                    <div className="flex flex-col gap-4 items-center justify-center py-4">
+                        <h2 className='text-5xl font-bold'>
+                            Seja Bem Vinde!
+                        </h2>
+                        <p className='text-xl'>
+                            Expresse aqui seus pensamentos e opniões
+                        </p>
 
-                    <div className='paragrafo'>
-                        <img 
-                            className='img'
-                            src="https://img.freepik.com/vetores-gratis/ilustracao-do-conceito-de-teletrabalho_52683-36163.jpg?size=626&ext=jpg&ga=GA1.1.1037085861.1697763769&semt=ais"
-                            alt="Icon de um escritório" 
+                        <div className="flex justify-around gap-4">
+                            <div className='rounded text-white 
+                                            border-white border-solid border-2 py-2 px-4'
+                                >
+                                Nova Postagem
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="flex justify-center ">
+                        <img
+                            src="https://i.imgur.com/fyfri1v.png"
+                            alt="Imagem Página Home"
+                            className='w-2/3'
                         />
                     </div>
                 </div>
